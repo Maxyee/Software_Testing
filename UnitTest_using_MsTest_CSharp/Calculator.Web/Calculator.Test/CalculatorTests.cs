2a874471006cf664fcc41d8b604dd851f834b662
Maxyee <eyaminkhan00@gmail.com>
2018-05-21 03:00:46 +0600

    Implementing 3 unit testing using MStest on the simple calculator project

---
+++ UnitTest_using_MsTest_CSharp/Calculator.Web/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Divide_PositiveNumbers_ReturnsPositiveQuotient()
+        {
+            //AAA
+            //Arrage
+            int expected = 5;
+            int numeriator = 20;
+            int denominator = 4;
+
+            //Act
+            int actual = Calculator.Divide(numeriator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_PositiveNumoratorAndNegativeDenominator_ReturnsNegativeQuotient()
+        {
+            //AAA
+            //Arrage
+            int expected = -5;
+            int numeriator = 20;
+            int denominator = -4;
+
+            //Act
+            int actual = Calculator.Divide(numeriator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_NegetiveNumbers_ReturnsPositiveQuotient()
+        {
+            //AAA
+            //Arrage
+            int expected = 5;
+            int numeriator = -20;
+            int denominator = -4;
+
+            //Act
+            int actual = Calculator.Divide(numeriator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTest_using_MsTest_CSharp/Calculator.Web/Calculator.Web/CalculatorPage.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalculatorPage.aspx.cs" Inherits="Calculator.Web.CalculatorPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+               <td>Numerator</td>
+                <td>
+                    <asp:TextBox ID="txtNumerator" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+               <td>Denominator</td>
+                <td>
+                    <asp:TextBox ID="txtDenominator" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+               <td>Result</td>
+                <td>
+                    <asp:Button ID="Divide" runat="server" Text="Divide" OnClick="Divide_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>Outpur</td>
+                <td>
+                    <asp:Label ID="lblResult" runat="server" Text="Outpur"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
